e479c8161dcbc7648795dfb3c4992da2993b165c
Akara Sucharitakul <asucharitakul@paypal.com>
2018-08-25 15:01:59 -0700

    Refined string search in matrix to never get lost.

--- src/main/scala/com/github/akara/playground/StringPathInMatrix.scala
+++ src/main/scala/com/github/akara/playground/StringPathInMatrix.scala
@@ -8,58 +8,67 @@ object StringPathInMatrix extends App {
     Vector('A', 'D', 'E', 'E')
   )
 
-  var scanned = List.empty[(Int, Int)]
+  //  var scanned = List.empty[(Int, Int)]
 
-  def stringPathInMatrix(s: String): Boolean = {
+  def stringPathInMatrix(s: String): Option[List[(Int, Int)]] = {
 
-    def matches(pos: (Int, Int), c: Char) = !scanned.contains(pos) && matrix(pos._1)(pos._2) == c
+    def matches(pos: (Int, Int), c: Char, scanned: List[(Int, Int)]) =
+      !scanned.contains(pos) && matrix(pos._1)(pos._2) == c
 
-    def found(c: Char): Boolean = {
+    def find(s: String, scanned: List[(Int, Int)]): Option[List[(Int, Int)]] = {
+
+      if (s.isEmpty) return Some(scanned)
 
       val currentPos = scanned.head
-
+      val c = s.head
       var checkPos = currentPos.copy(_2 = currentPos._2 - 1) // left
-      if (checkPos._2 >= 0 && matches(checkPos, c)) {
-        scanned = checkPos :: scanned
-        return true
+      if (checkPos._2 >= 0 && matches(checkPos, c, scanned)) {
+        println(s"Found $checkPos")
+        val result = find(s.tail, checkPos :: scanned)
+        if (result.isDefined) return result
       }
       checkPos = currentPos.copy(_1 = currentPos._1 - 1) // up
-      if (checkPos._1 >= 0 && matches(checkPos, c)) {
-        scanned = checkPos :: scanned
-        return true
+      if (checkPos._1 >= 0 && matches(checkPos, c, scanned)) {
+        println(s"Found $checkPos")
+
+        val result = find(s.tail, checkPos :: scanned)
+        if (result.isDefined) return result
       }
       checkPos = currentPos.copy(_2 = currentPos._2 + 1) // right
-      if (checkPos._2 < matrix(checkPos._1).size && matches(checkPos, c)) {
-        scanned = checkPos :: scanned
-        return true
+      if (checkPos._2 < matrix(checkPos._1).size && matches(checkPos, c, scanned)) {
+        println(s"Found $checkPos")
+        val result = find(s.tail, checkPos :: scanned)
+        if (result.isDefined) return result
       }
       checkPos = currentPos.copy(_1 = currentPos._1 + 1) // down
-      if (checkPos._1 < matrix.size && matches(checkPos, c)) {
-        scanned = checkPos :: scanned
-        return true
+      println(s"Found $checkPos")
+      if (checkPos._1 < matrix.size && matches(checkPos, c, scanned)) {
+        val result = find(s.tail, checkPos :: scanned)
+        if (result.isDefined) return result
       }
-      false
+      None
     }
 
-    def scan(str: String): Boolean = {
+    def scan(str: String): Option[List[(Int, Int)]] = {
       for {
         i <- matrix.indices
         j <- matrix(i).indices
       } {
         if (str.head == matrix(i)(j)) {
-          scanned = (i, j) :: Nil
-
-          if (str.tail.forall(found)) return true
+          println(s"Found ($i,$j)")
+          return find(str.tail, (i, j) :: Nil)
         }
       }
-      false
+      None
     }
 
-    scan(s)
-
+    scan(s).map(_.reverse)
   }
 
   val s = "BCCED"
-  if (stringPathInMatrix(s)) println(s"Found $s: ${scanned.reverse}")
-  else println("Not found")
+
+  stringPathInMatrix(s) match {
+    case Some(list) => println(s"Found $s: $list")
+    case None => println("Not found")
+  }
 }
