39456670fc8c7be074db1170947969515d06c017
Akara Sucharitakul <asucharitakul@paypal.com>
2018-08-25 15:10:53 -0700

    Refined string search in matrix to never get lost.

--- src/main/scala/com/github/akara/playground/StringPathInMatrix.scala
+++ src/main/scala/com/github/akara/playground/StringPathInMatrix.scala
@@ -17,31 +17,26 @@ object StringPathInMatrix extends App {
 
     def find(s: String, scanned: List[(Int, Int)]): Option[List[(Int, Int)]] = {
 
-      if (s.isEmpty) return Some(scanned)
+      if (s.isEmpty && scanned.nonEmpty) return Some(scanned)
 
       val currentPos = scanned.head
       val c = s.head
       var checkPos = currentPos.copy(_2 = currentPos._2 - 1) // left
       if (checkPos._2 >= 0 && matches(checkPos, c, scanned)) {
-        println(s"Found $checkPos")
         val result = find(s.tail, checkPos :: scanned)
         if (result.isDefined) return result
       }
       checkPos = currentPos.copy(_1 = currentPos._1 - 1) // up
       if (checkPos._1 >= 0 && matches(checkPos, c, scanned)) {
-        println(s"Found $checkPos")
-
         val result = find(s.tail, checkPos :: scanned)
         if (result.isDefined) return result
       }
       checkPos = currentPos.copy(_2 = currentPos._2 + 1) // right
       if (checkPos._2 < matrix(checkPos._1).size && matches(checkPos, c, scanned)) {
-        println(s"Found $checkPos")
         val result = find(s.tail, checkPos :: scanned)
         if (result.isDefined) return result
       }
       checkPos = currentPos.copy(_1 = currentPos._1 + 1) // down
-      println(s"Found $checkPos")
       if (checkPos._1 < matrix.size && matches(checkPos, c, scanned)) {
         val result = find(s.tail, checkPos :: scanned)
         if (result.isDefined) return result
@@ -55,7 +50,6 @@ object StringPathInMatrix extends App {
         j <- matrix(i).indices
       } {
         if (str.head == matrix(i)(j)) {
-          println(s"Found ($i,$j)")
           return find(str.tail, (i, j) :: Nil)
         }
       }
